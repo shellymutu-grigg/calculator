4afe9526ba69525e690a2cf8aa599d557d85efb8
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-09-28 19:48:46 +1300

    Update pseudocode branch for pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,32 @@
+//HTML 
+//Create calculator class to define the look of the calculator
+//.calculator 
+//Title, Tagline & Answer IDs
+
+//CSS
+//Import the necessary stylesheets
+//Define the necessary colours
+//Define body & html properties
+//Define .calculator class
+//Define p ids to define title & tagline
+//Define text input
+//Define buttons
+//Define .ctrl class
+//Define .tall class
+
+//JS
+//Define global variables
+//Use JQuery to determine action onclick of a button
+//Check if the input is a number or not and if OK add to the temp string variable
+//Use JQuery to assign to the answer element in HTML using the .val
+//Check for selection of either AC or CE buttons and empty answer element
+//Define multiply functionality using text versus mathematical operator
+//Define division functionality using text versus mathematical operator
+//Define equals functionality
+//  using addition
+//  using minus
+//  using multiplication
+//  using division
+//Cater for - symbol
+
+
